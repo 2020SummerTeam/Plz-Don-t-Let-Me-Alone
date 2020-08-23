72c4290bb233a6bd1e38f12c3557314f0d37736a
u-nij <u.nij0509@gmail.com>
2020-08-23 19:21:40 +0900

    Forest9 추가
    Forest9 스크립트
    - 드래그로 플랫폼 이동
    - 스테이지 클리어 전 플레이어 컨트롤

---
+++ UnityProjectTeam3/Assets/Script/StageManager/Forest/Forest9.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Forest9 : MonoBehaviour
+{
+    public GameObject movingPlatform;
+    private Transform mTr;
+    private Vector3 pos, oldpos;
+    private float min_x_scale;
+    private float max_x_scale;
+    public float distance = 2.5f;
+
+    // 플레이어 컨트롤
+    private Rigidbody2D pRB;
+    [SerializeField]
+    private Vector2 pJumpVector;
+    private Animator pAnim;
+    private int count = 1;   // 한번만 점프하도록 제한
+    public float cooltime = 0.5f;  // 점프 쿨타임
+    private float t;
+
+    PlayerCtrl playerCtrl; // 스테이지 클리어시 playerCtrl 스크립트 활성화
+    public GameObject player;
+    ParentsCtrl parents;
+
+    // 버튼 컨트롤 // 플레이어와 부딪힐 시 부모 위 플랫폼 활성화
+    public bool buttonTriggerd = false;
+    public GameObject platform;
+
+    public Transform originObj;
+    public Transform reflectObj;
+
+
+    void Start()
+    {
+        mTr = movingPlatform.GetComponent<Transform>();
+        max_x_scale = mTr.position.x + distance;
+        min_x_scale = mTr.position.x - distance;
+
+        parents = GameObject.Find("Parents").GetComponent<ParentsCtrl>();
+        pRB = player.GetComponent<Rigidbody2D>();
+        playerCtrl = player.GetComponent<PlayerCtrl>();
+        pAnim = player.GetComponent<Animator>();
+        t = cooltime;
+    }
+
+    void Update()
+    {
+        if (parents.stageClear) // 클리어 이벤트 발동을 위해
+        {
+            playerCtrl.enabled = true;
+            this.enabled = false;
+        }
+        else
+        {
+            if (t >= 0)
+            {
+                count = 1;
+                t -= Time.deltaTime;
+            }
+            else
+            {
+                if (count == 1)
+                {
+                    pRB.AddForce(pJumpVector, ForceMode2D.Impulse); // 점프
+                    count--;
+                }
+                t = cooltime;
+            }
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                pos = mTr.position;
+            }
+            if (Input.GetMouseButton(0))
+            {
+                oldpos = pos;
+                pos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
+                mTr.position = new Vector3(mTr.position.x + (pos.x - oldpos.x), mTr.position.y, 0);
+
+                // MovingPlatform 범위 제한
+                if (mTr.position.x >= max_x_scale)
+                {
+                    mTr.position = new Vector3(max_x_scale, mTr.position.y, mTr.position.z);
+                }
+                else if (mTr.position.x <= min_x_scale)
+                {
+                    mTr.position = new Vector3(min_x_scale, mTr.position.y, mTr.position.z);
+                }
+
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+
+            buttonTriggerd = true;
+            platform.SetActive(true);
+            Debug.Log(reflectObj.position);
+        }
+    }
+}
---
+++ UnityProjectTeam3/Assets/Script/reflectPlayer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class reflectPlayer : MonoBehaviour
+{
+    // 플레이어가 부딪혔을 때, 반사각을 구해 플레이어를 반대편으로 튕겨냄
+
+    public Rigidbody2D player;
+
+    private Vector2 reflectPos;
+    private bool isTrigger = false;
+
+    public int direction; // y축을 뒤집기 위해 movingPlatform일 경우 -1(아래로), button일 경우 1(위로)으로 설정
+    public Vector2 innormal;
+    
+
+    void FixedUpdate()
+    {
+        if (isTrigger) // 연달아 실행하지 않도록
+        {
+            player.AddForce(reflectPos);
+            isTrigger = false;
+        }
+
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            innormal = new Vector2(collision.contacts[0].point.x, collision.contacts[0].point.y * direction); // player와 부딪힌 곳의 좌표
+            reflectPos = Vector2.Reflect(collision.transform.position, innormal); // 반사각을 구하는 함수
+            isTrigger = true;
+        }
+    }
+}

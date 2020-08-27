8a93a05c0c2f1ed6ab9e9409194be313e2b8a6b2
eomjimin <68543910+eomjimin@users.noreply.github.com>
2020-08-27 21:46:13 +0900

    eomjimin

--- UnityProjectTeam3/Assets/Script/WordDrag.cs
+++ UnityProjectTeam3/Assets/Script/WordDrag.cs
@@ -6,54 +6,7 @@ using UnityEngine;
 
 public class WordDrag : MonoBehaviour
 {
-    private GameObject target;
-
-
-
-    void FixedUpdate()
-
-    {
-
-        Vector3 scrSpace = Camera.main.WorldToScreenPoint(transform.position);
-        Vector3 offset = transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, scrSpace.z));
-
-        if (Input.GetMouseButtonDown(0))
-        {
-
-            CastRay();
-
-            if (target == this.gameObject)
-            {
-                OnMouseDown();
-            }
-
-        }
-
-    }
-
-
-
-    void CastRay() // 유닛 히트처리 부분.  레이를 쏴서 처리합니다. 
-
-    {
-
-        target = null;
-
-
-
-        Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-        RaycastHit2D hit = Physics2D.Raycast(pos, Vector2.zero, 0f);
-
-
-
-        if (hit.collider != null)
-        { 
-            target = hit.collider.gameObject;  //히트 된 게임 오브젝트를 타겟으로 지정
-
-        }
-
-    }
+    public GameObject player;//draw character
     IEnumerator OnMouseDown() // 단어 드래그 드랍
     {
         Vector3 scrSpace = Camera.main.WorldToScreenPoint(transform.position);
@@ -62,6 +15,7 @@ public class WordDrag : MonoBehaviour
 
         while (Input.GetMouseButton(0))
         {
+            player.GetComponent<PlayerCtrl>().enabled = false; //단어를 터치한 상태에는 PlayerCtrl 스크립트 비활성화
             Vector3 curScreenSpace = new Vector3(Input.mousePosition.x, Input.mousePosition.y, scrSpace.z);
             Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenSpace) + offset;
             Vector3 worldpos = curPosition;
@@ -78,7 +32,6 @@ public class WordDrag : MonoBehaviour
             transform.position = worldpos;
             yield return null;
         }
+        player.GetComponent<PlayerCtrl>().enabled = true; //단어를 터치하지 않을때는 스크립트 활성화
     }
 }
-
-
